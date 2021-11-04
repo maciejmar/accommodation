b2a72f48db71a249bae70ab583cf5a28947919fe
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2021-11-04 17:19:18 +0100

    some preferences changed again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Hotels from './components/Hotels/Hotels.js';
+
+
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu/>
+      <Hotels/>
     </div>
   );
 }
---
+++ src/components/Hotels/Hotels.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+class Hotels extends Component {
+  render ()
+  {
+      return (
+    <div>
+        hotels
+    </div>
+  )
+  }
+}
+
+
+
+export default Hotels;
