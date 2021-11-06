eefb6fdcf709a36b0ee69b6f52eac54bb943c813
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2021-11-06 14:30:44 +0100

    search button events

--- src/components/Header/SearchBar/SearchBar.js
+++ src/components/Header/SearchBar/SearchBar.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar () {
+    const [term, setTerm] = useState('');
+    const search= () => {
+        console.log('szukaj!', term);
+    }
+
+    const updateTerm = (e) => {
+        setTerm(e.target.value);
+    }
+    const onKeyDwonHandler =  (e) => {
+        if(e.key === 'enter') search(); 
+    }
+
     return(
    <div className='d-flex'>
        
-            <input className='form-control'
+            <input className='form-control' value={term} 
+            onChange={updateTerm} onKeyDown={onKeyDwonHandler}
             type="text" placeholder="Searching..." />
-            <button className="ml-1 btn btn-secondary"> GO! </button> 
+            <button onClick = {search} className="ml-1 btn btn-secondary"> GO! </button> 
         
 
     </div>
